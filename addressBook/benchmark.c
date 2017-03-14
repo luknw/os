a5847248ce0656ad88a0d72ea7e5c4e3bb7e708c
Lukasz Nawojowski <luknwpl@gmail.com>
2017-03-14 01:55:12 +0100

    Add benchmark for TreeBook

--- addressBook/benchmark.c
+++ addressBook/benchmark.c
@@ -5,7 +5,7 @@
 #include "timer.h"
 #include "libAddressBook/addressBook.h"
 
-static const int CONTACT_COUNT = 1000;
+static const int CONTACT_COUNT = 16000;
 static const int BUFFER_SIZE = 128;
 static const char *DELIMITERS = ",\n";
 
@@ -53,7 +53,6 @@ static void benchmarkListAddressBook(ListAddressBook **book, Contact **contacts)
     MEASURE_TIME("ListAddressBook - adding first contact", ListAddressBook_addContact(*book, contacts[0]);)
     avgAddingClocks = ClockInterval_add(avgAddingClocks, _clockInterval);
 
-
     MEASURE_TIME("ListAddressBook - adding almost all contacts",
                  for (int i = 1; i < CONTACT_COUNT - 1; ++i) {
                      ListAddressBook_addContact(*book, contacts[i]);
@@ -85,11 +84,62 @@ static void benchmarkListAddressBook(ListAddressBook **book, Contact **contacts)
 
     MEASURE_TIME("ListAddressBook - deleting optimistic",
                  ListAddressBook_removeContact(*book, (*book)->contacts->next->contact);)
-
 }
 
-static void benchmarkTreeAddressBook(TreeAddressBook *book, Contact **contacts) {
-    //todo
+static void benchmarkTreeAddressBook(TreeAddressBook **book, Contact **contacts) {
+    INIT_MEASURE_TIME()
+
+    MEASURE_TIME("TreeAddressBook - creating", *book = TreeAddressBook_new();)
+
+    ClockInterval avgAddingClocks;
+    avgAddingClocks.realClocks = 0;
+    avgAddingClocks.userClocks = 0;
+    avgAddingClocks.systemClocks = 0;
+
+    MEASURE_TIME("TreeAddressBook - adding first contact", TreeAddressBook_addContact(*book, contacts[0]);)
+    avgAddingClocks = ClockInterval_add(avgAddingClocks, _clockInterval);
+
+    MEASURE_TIME("TreeAddressBook - adding almost all contacts",
+                 for (int i = 1; i < CONTACT_COUNT - 1; ++i) {
+                     TreeAddressBook_addContact(*book, contacts[i]);
+
+                 })
+    avgAddingClocks = ClockInterval_add(avgAddingClocks, _clockInterval);
+
+    MEASURE_TIME("TreeAddressBook - adding last contact",
+                 TreeAddressBook_addContact(*book, contacts[CONTACT_COUNT - 1]);)
+    avgAddingClocks = ClockInterval_add(avgAddingClocks, _clockInterval);
+
+    SecondsInterval avgAddingSeconds = ClockInterval_toSeconds(avgAddingClocks);
+    avgAddingSeconds = SecondsInterval_divSeconds(avgAddingSeconds, (Seconds) CONTACT_COUNT);
+
+    SecondsInterval_print(avgAddingSeconds, "TreeAddressBook - adding average");
+
+    MEASURE_TIME("TreeAddressBook - finding optimistic",
+                 TreeAddressBook_findContact(*book, (*book)->key,
+                                             Contact_getValue((*book)->contacts->contact, (*book)->key));)
+
+    MEASURE_TIME("TreeAddressBook - deleting optimistic",
+                 TreeAddressBook_removeContact(*book, (*book)->contacts->contact);)
+
+    MEASURE_TIME("TreeAddressBook - rearranging", TreeAddressBook_rearrangeByKey(book, PHONE);)
+
+    MEASURE_TIME("TreeAddressBook - deallocating", TreeAddressBook_delete(*book);)
+
+
+    *book = TreeAddressBook_new();
+    MEASURE_TIME("TreeAddressBook - adding pessimistic data",
+                 for (int i = 0; i < CONTACT_COUNT - 1; ++i) {
+                     TreeAddressBook_addContact(*book, contacts[0]);
+                 }
+                         TreeAddressBook_addContact(*book, contacts[2]);)
+
+    MEASURE_TIME("TreeAddressBook - finding pessimistic",
+                 TreeAddressBook_findContact(*book, (*book)->key,
+                                             Contact_getValue(contacts[2], (*book)->key));)
+
+    MEASURE_TIME("TreeAddressBook - deleting pessimistic",
+                 TreeAddressBook_removeContact(*book, contacts[2]);)
 }
 
 
@@ -100,11 +150,13 @@ int main(void) {
     ListAddressBook *listBook = NULL;
     benchmarkListAddressBook(&listBook, contacts);
 
-//    TreeAddressBook *treeBook = NULL;
-//    benchmarkTreeAddressBook(treeBook, contacts);
+    printf("\n");
+
+    TreeAddressBook *treeBook = NULL;
+    benchmarkTreeAddressBook(&treeBook, contacts);
 
     ListAddressBook_delete(listBook);
-//    TreeAddressBook_delete(treeBook);
+    TreeAddressBook_delete(treeBook);
 
     cleanup(contacts);
 
