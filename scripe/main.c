659fbce7bb73de7c33a86d87d32ca2a766796582
Lukasz Nawojowski <luknwpl@gmail.com>
2017-03-25 21:00:19 +0100

    Add limits

--- scripe/main.c
+++ scripe/main.c
@@ -21,6 +21,15 @@ static char *lineBuffer;
 static unsigned int lineCount = 1;
 
 
+enum CmdLimitIndices {
+    MEMORY,
+    CPU
+};
+
+static struct rlimit cmdLimits[] = {{RLIMIT_AS},
+                                    {RLIMIT_CPU}};
+
+
 EnvCmd parseEnvCmd(char *cmdLine) {
     EnvCmd cmd;
     cmd.varName = strtok(cmdLine + 1, WHITESPACE);
@@ -75,12 +84,24 @@ void performEnvCmd(EnvCmd cmd) {
     }
 }
 
+static void setResourceLimits() {
+    if (setrlimit(RLIMIT_AS, &cmdLimits[MEMORY]) == -1) {
+        perror("Error setting memory limit");
+        exit(EXIT_FAILURE);
+    }
+    if (setrlimit(RLIMIT_CPU, &cmdLimits[CPU]) == -1) {
+        perror("Error setting cpu time limit");
+        exit(EXIT_FAILURE);
+    }
+}
+
 void performExecCmd(ExecCmd cmd) {
     int status = 0;
     struct rusage rusage;
 
     pid_t pid = fork();
     if (pid == 0) {
+        setResourceLimits();
         if (execvp(cmd.cmd, cmd.argv) == -1) {
             fprintf(stderr, "Cannot execute %s: %s\n", cmd.cmd, strerror(errno));
             exit(EXIT_FAILURE);
@@ -94,10 +115,11 @@ void performExecCmd(ExecCmd cmd) {
             exit(WEXITSTATUS(status));
         }
 
-        fprintf(stderr, "%-8s%-10d%-15s%4li%s%-10li%-15s%4li%s%-10li\n",
+        fprintf(stderr, "%-8s%-10d%20s%9li%-10s%-15s%4li%s%li%-5s%-15s%4li%s%li%-5s\n",
                 "PID:", pid,
-                "User time:", rusage.ru_utime.tv_sec, ".", rusage.ru_utime.tv_usec,
-                "System time:", rusage.ru_stime.tv_sec, ".", rusage.ru_stime.tv_usec);
+                "Max resident memory:", rusage.ru_maxrss, " kB",
+                "User time:", rusage.ru_utime.tv_sec, ".", rusage.ru_utime.tv_usec, " s",
+                "System time:", rusage.ru_stime.tv_sec, ".", rusage.ru_stime.tv_usec, " s");
 
         safe_free(cmd.argv);
     } else {
@@ -117,25 +139,43 @@ void interpretLine(char *line, ssize_t lineLen) {
     }
 }
 
-void interpret(FILE *script) {
-    while (safe_getline_content(&lineBuffer, &lineBufferSize, script) != EOF) {
+void interpret(char *filePath) {
+    FILE *sScript = safe_fopen(filePath, "r");
+
+    while (safe_getline_content(&lineBuffer, &lineBufferSize, sScript) != EOF) {
         interpretLine(lineBuffer, lineBufferSize);
         lineCount++;
     }
+
+    safe_fclose(sScript);
 }
 
 
+void findResourceLimits(int argc, char **argv) {
+    if (argc >= 4) {
+        cmdLimits[MEMORY].rlim_cur = cmdLimits[MEMORY].rlim_max = (rlim_t) atoll(argv[2]);
+        cmdLimits[CPU].rlim_cur = cmdLimits[CPU].rlim_max = (rlim_t) atoll(argv[3]);
+        return;
+    }
+
+    if (getrlimit(RLIMIT_AS, &cmdLimits[MEMORY]) == -1) {
+        perror("Error getting resource limit");
+        exit(EXIT_FAILURE);
+    }
+    if (getrlimit(RLIMIT_CPU, &cmdLimits[CPU]) == -1) {
+        perror("Error getting resource limit");
+        exit(EXIT_FAILURE);
+    }
+}
+
 int main(int argc, char **argv) {
-    if (argc != 2) {
+    if (argc < 2) {
         printf("%s\n", USAGE);
         return 0;
     }
 
-    FILE *sScript = safe_fopen(argv[1], "r");
-
-    interpret(sScript);
-
-    safe_fclose(sScript);
+    findResourceLimits(argc, argv);
+    interpret(argv[1]);
 
     return 0;
 }
---
+++ scripe/timeLimitTester.c
@@ -0,0 +1,9 @@
+//
+// Created by luknw on 3/25/17.
+//
+
+
+int main() {
+    for (int i = 0;; ++i);
+    return 0;
+}
